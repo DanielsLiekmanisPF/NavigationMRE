7d1d294b0b15a7c751c1ca23d6dadacafc7fe254
Daniels Liekmanis <daniels.liekmanis@printful.com>
2024-02-12 11:09:36 +0200

    Initial Commit

---
+++ NavigationMRE/NavigationMREApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationMREApp.swift
+//  NavigationMRE
+//
+//  Created by Daniels Liekmanis on 12/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationMREApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
